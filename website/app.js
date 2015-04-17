45d8fc8d019bbd404e142c7db6b7109a7ac6f865
Ellen Steube <ellensteube@gmail.com>
2015-04-17 16:04:40 -0600

    Added jQuery
    Added jQuery, edited html files, added css and gif

---
+++ website/app.js
@@ -0,0 +1,14 @@
+//$("button").click(function(){
+//    $("#div1").fadeIn();
+//    $("#div2").fadeIn("slow");
+//    $("#div3").fadeIn(3000);
+//});
+
+$(document).ready(function() {
+    $(".hide").click(function(){
+        $(".ellenpic").hide();
+    }); 
+    $(".show").click(function(){
+        $(".ellenpic").show();
+    }); 
+});
